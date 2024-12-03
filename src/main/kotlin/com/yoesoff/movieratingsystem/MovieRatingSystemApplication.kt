80251872fa1893395144ef69ac317155a6c53e94
Yusuf Ibrahim <yusuf@softwareseni.com>
2024-12-03 10:40:02 +0700

    First init

---
+++ src/main/kotlin/com/yoesoff/movieratingsystem/MovieRatingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.yoesoff.movieratingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieRatingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieRatingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/yoesoff/movieratingsystem/MovieRatingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoesoff.movieratingsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieRatingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
